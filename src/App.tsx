7717cf5ccc25fb691b38dc6f5055675ffd651502
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2024-07-12 14:07:00 +0200

    Hook into beforeUnload to subvert anchor element that perform other queries
    One issue with this approach is that for security reasons the user
    agent will show a pop-up. The user is required to cancel the
    navigation to perform the search which is mega unintuitive.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import * as Result from "./Result";
+import * as Router from "react-router-dom";
 
-export const App = (): React.ReactElement => {
+const isHTMLLinkElement = (x: unknown): x is HTMLLinkElement =>
+  Boolean(x && typeof x === "object" && "href" in x);
+
+export const Main = (): React.ReactElement => {
   const [inp, setInp] = React.useState<string>("");
   const [query, setQuery] = React.useState<string>("");
+  Router.useBeforeUnload((event): void => {
+    if (!isHTMLLinkElement(document.activeElement)) return;
+    const { searchParams } = new URL(document.activeElement.href);
+    const newInp = searchParams.get("SearchableText");
+    if (newInp === null) return;
+    setInp(newInp);
+    setQuery(newInp);
+    event.preventDefault();
+  });
   return (
     <>
       <form
@@ -19,3 +32,9 @@ export const App = (): React.ReactElement => {
     </>
   );
 };
+
+export const App = (): React.ReactElement => (
+  <Router.BrowserRouter>
+    <Main />
+  </Router.BrowserRouter>
+);
--- src/cache.ts
+++ src/cache.ts
@@ -29,18 +29,17 @@ export type Parse<T> = (s: string) => T;
 
 export type Serialize<T> = [Parse<T>, Encode<T>];
 
-export const cached = <T>(
-  [parse, encode]: Serialize<T>,
-  k: string,
-  f: (x: string) => Promise<T>,
-): ((x: string) => Promise<T>) => {
-  const cache = loadCache(k);
-
-  return async (x: string): Promise<T> => {
+export const cached =
+  <T>(
+    [parse, encode]: Serialize<T>,
+    k: string,
+    f: (x: string) => Promise<T>,
+  ): ((x: string) => Promise<T>) =>
+  async (x: string): Promise<T> => {
+    const cache = loadCache(k);
     const c = cache[x];
     if (c) return parse(c);
     const y = await f(x);
     store(k, { ...cache, [x]: encode(y) });
     return y;
   };
-};
